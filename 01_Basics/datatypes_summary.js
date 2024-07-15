f4e0af715cd2ee7c706d4cd75f0533157fa816d1
Pranav-javali <108029771+Pranav-javali@users.noreply.github.com>
2024-07-15 11:08:56 +0000

    3_4_datatypes

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.5;
+
+const isLoggedIn = false;
+const outSideTemp = null;
+
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 3455756674646767667n;
+
+//Reference type(non-primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Ironman", "Thor", "Spiderman"]
+
+let myObj = { name: "Pranav",
+            age: 23, }
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction)
+
