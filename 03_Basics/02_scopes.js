1c271fcdbb43fa1282c40c2dbea2e7ae3e26b1cf
Pranav-javali <108029771+Pranav-javali@users.noreply.github.com>
2024-08-06 11:41:01 +0000

    02

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,60 @@
+
+//var c = 300
+let a = 100
+
+if(true)
+{
+    let a =10
+    const b = 20
+    var c = 30
+    //console.log("Inner ",a);
+
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+//22
+
+function one(){
+    const username = "Pranav"
+    function two()
+    {
+        const website = " youtube"
+        //console.log(username)
+    }
+    //console.log(website)
+    //two()
+}
+//one()
+
+
+if(true)
+{
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+        //console.log(username+website)
+    }
+    //console.log(website);
+}
+//console.log(username)
+
+
+//+++++++++Interesting++++++++++++++++
+
+//normal function
+
+console.log(addOne(5))
+function addOne(num1){
+    return num1 +1
+}
+//console.log(addOne(5))
+
+//console.log(addTwo(5))
+const addTwo = function(num)
+{
+    return num +2
+}
+//console.log(addTwo(5))
