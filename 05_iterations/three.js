5a71e9987f9ef6a74c04155a07a82ee095dac8e8
Pranav-javali <108029771+Pranav-javali@users.noreply.github.com>
2024-08-13 09:26:55 +0000

    29

---
+++ 05_iterations/three.js
@@ -0,0 +1,50 @@
+// for of
+
+//syntax
+// for (const element of object) {
+    
+// }
+
+//array
+const arr = [1,2,3,4,5]
+
+// for(const num of arr){
+//     console.log(num)
+// }
+
+
+//on string
+const greetings = "Hello World!"
+
+// for (const greet of greetings) {
+//     console.log(`Each char id ${greet}`)
+// }
+
+//Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+//Adding duplicate
+map.set('IN', "India")
+
+//console.log(map);
+
+// for (const key of map) {
+//     console.log(key)
+// }
+//or
+// for (const [key,value] of map) {
+//     console.log(key,":-",value)
+// }
+
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+for (const [key,value] of myObject) {
+    console.log(key,":-",value)
+}
