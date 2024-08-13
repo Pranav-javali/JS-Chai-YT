59d8c6e51ae0a70c25520af1dbb63480a0feeef7
Pranav-javali <108029771+Pranav-javali@users.noreply.github.com>
2024-08-13 10:14:39 +0000

    29

---
+++ 05_iterations/four.js
@@ -0,0 +1,37 @@
+//for in 
+
+const myObject = {
+    js: 'Javascript',
+    cpp : 'C++',
+    rb: "ruby",
+    swift: "Swift by apple"
+}
+
+// for (const key in myObject) {
+//     console.log(key)
+// }
+
+// for (const key in myObject) {
+//         //console.log(myObject[key])
+//         console.log(`${key} shortcut for ${myObject[key]}`)
+// }
+
+
+const programming = ["js","rb","py","java","cpp"]
+
+// for (const key in programming) {
+//     console.log(key)
+// }
+
+// for (const key in programming) {
+//     console.log(programming[key])
+// }
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+// for (const key in map) {
+//     console.log(key)
+// }
