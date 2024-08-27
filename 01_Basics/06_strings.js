2a313899b8a14b5f9a0ca684e5d654d356beac64
Pranav-javali <108029771+Pranav-javali@users.noreply.github.com>
2024-08-27 07:40:43 +0000

    06

--- 01_Basics/06_strings.js
+++ 01_Basics/06_strings.js
@@ -4,3 +4,28 @@ const repo = 29
 //console.log(name+repo+" value")
 
 console.log(`Hello my name is ${name} and my repo count is ${repo} `)
+
+const gameName = new String("Pranavrj")
+
+console.log(gameName[0])
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('r'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne = "    dwccd    "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://pranav.com/pranav%20javali"
+
+console.log(url.replace('%20','_'))
+
+console.log(url.includes('pranav'))
