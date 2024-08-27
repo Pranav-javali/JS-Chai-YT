cfa55484c436398f53236af6ef5ffcb76512b01f
Pranav-javali <108029771+Pranav-javali@users.noreply.github.com>
2024-08-27 18:00:00 +0000

    new

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -97,3 +97,12 @@ handleObject({
     username: "sam",
     price: 299
 })
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]))
